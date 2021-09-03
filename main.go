710e030f71220ef573517004312298f04b4460ed
Hudoshnik <c4nsero@mail.ru>
2021-09-04 01:06:35 +0300

    new design

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func newMapaStr(o [8][8]string) mapa {
 }
 
 func (m mapa) writeMap() {
+	fmt.Println("\033[2J") //only in bash
 	for i := 0; i < m.size; i++ {
 		for j := 0; j < m.size; j++ {
 			o := m.map0[i][j]
@@ -51,14 +52,14 @@ func main() {
 		typeOfObj: "o",
 	}
 	psevdoMapStr := [8][8]string{
-		{"Г", "¯", "¯", "¯", "¯", "¯", "¯", "˥"},
-		{"|", " ", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", " ", "|"},
-		{"|", " ", " ", " ", " ", " ", " ", "|"},
-		{"L", "_", "_", "_", "_", "_", "_", "˩"},
+		{"*", "*", "*", "*", "*", "*", "*", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", "*", "*", "*", "*", "*", "*", "*"},
 	}
 	psevdoMap := newMapaStr(psevdoMapStr)
 	for {
@@ -66,30 +67,26 @@ func main() {
 			for i := 1; i < 7; i++ {
 				psevdoMap.movePlayer(j, i, &player)
 				psevdoMap.writeMap()
-
 			}
-			for i := 5; i > 0; i-- {
+			for i := 6; i > 0; i-- {
 				psevdoMap.movePlayer(j, i, &player)
 				psevdoMap.writeMap()
-
 			}
 		}
 		for j := 6; j > 0; j-- {
 			for i := 1; i < 7; i++ {
 				psevdoMap.movePlayer(j, i, &player)
 				psevdoMap.writeMap()
-
 			}
-			for i := 5; i > 0; i-- {
+			for i := 6; i > 0; i-- {
 				psevdoMap.movePlayer(j, i, &player)
 				psevdoMap.writeMap()
-
 			}
 		}
 	}
 }
 
-//----------------
+//---------------- old things
 /*
 
 
@@ -110,12 +107,12 @@ func newMapa(o [3][3]obj) mapa {
 	o01 := obj{
 		x:         0,
 		y:         1,
-		typeOfObj: "|",
+		typeOfObj: "*",
 	}
 	o10 := obj{
 		x:         1,
 		y:         0,
-		typeOfObj: "|",
+		typeOfObj: "*",
 	}
 	o11 := obj{
 		x:         1,
@@ -136,3 +133,32 @@ func newMapa(o [3][3]obj) mapa {
 	fmt.Println("101")
 	fmt.Println("\033[2J")
 	fmt.Println("102")*/
+
+//-------------------------console clear
+/*
+
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func CallClear() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
+*/
