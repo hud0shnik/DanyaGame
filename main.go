6c32b27bd658a7161a84c5e13b3fd69a22b26dbe
Hudoshnik <c4nsero@mail.ru>
2021-09-04 18:46:58 +0300

    + second room

--- main.go
+++ main.go
@@ -2,26 +2,33 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 type obj struct {
-	x         int
-	y         int
-	typeOfObj string
+	x        int
+	y        int
+	typeChar string
+}
+
+type character struct {
+	x        int
+	y        int
+	typeChar string
+	hp       int
+	pocket   map[string]int
 }
 
 type room struct {
 	size   int
-	matrix [8][8]obj
+	matrix [16][16]obj
 }
 
-func newRoomStr(o [8][8]string) room {
+func newRoomStr(o [16][16]string) room {
 	var result room
 	result.size = len(o)
 	for i := 0; i < result.size; i++ {
 		for j := 0; j < result.size; j++ {
-			result.matrix[i][j].typeOfObj = o[i][j]
+			result.matrix[i][j].typeChar = o[i][j]
 			result.matrix[i][j].x = i
 			result.matrix[i][j].y = j
 		}
@@ -34,157 +41,144 @@ func (m room) writeMap() {
 	for i := 0; i < m.size; i++ {
 		for j := 0; j < m.size; j++ {
 			o := m.matrix[i][j]
-			fmt.Print(o.typeOfObj)
+			fmt.Print(o.typeChar)
 		}
 		fmt.Print("\n")
 	}
-	time.Sleep(time.Second / 60)
 }
-func (m *room) movePlayer(x int, y int, pl *obj) {
-	if m.matrix[x][y].typeOfObj == "*" {
-
-	} else {
-		m.matrix[pl.x][pl.y].typeOfObj = " "
-		m.matrix[x][y].typeOfObj = pl.typeOfObj
+func movePlayer(x int, y int, pl *character, m *room) bool { //bool true - new lvl
+	if m.matrix[x][y].typeChar == " " {
+		m.matrix[pl.x][pl.y].typeChar = " "
+		m.matrix[x][y].typeChar = pl.typeChar
 		pl.x, pl.y = x, y
+	} else if m.matrix[x][y].typeChar == "⊞" { //Trapdoor
+		if pl.pocket["key"] > 0 {
+			m.matrix[pl.x][pl.y].typeChar = " "
+			m.matrix[x][y].typeChar = pl.typeChar
+			pl.x, pl.y = x, y
+			pl.pocket["key"]--
+			return true
+		}
+	} else if m.matrix[x][y].typeChar == "⚿" {
+		m.matrix[pl.x][pl.y].typeChar = " "
+		m.matrix[x][y].typeChar = pl.typeChar
+		pl.x, pl.y = x, y
+		pl.pocket["key"]++
 	}
 	m.writeMap()
+	return false
 }
-func main() {
-	player := obj{
-		x:         1,
-		y:         1,
-		typeOfObj: "o",
-	}
+
+func game(player *character, currentRoom *room) bool {
+	movePlayer(1, 1, player, currentRoom)
 	var key string
-	psevdoMapStr := [8][8]string{
-		{"*", "*", "*", "*", "*", "*", "*", "*"},
-		{"*", " ", " ", " ", " ", " ", " ", "*"},
-		{"*", " ", " ", " ", " ", " ", " ", "*"},
-		{"*", " ", " ", " ", " ", " ", " ", "*"},
-		{"*", " ", " ", " ", " ", " ", " ", "*"},
-		{"*", " ", " ", " ", " ", " ", " ", "*"},
-		{"*", " ", " ", " ", " ", " ", " ", "*"},
-		{"*", "*", "*", "*", "*", "*", "*", "*"},
-	}
-	psevdoMap := newRoomStr(psevdoMapStr)
-	psevdoMap.movePlayer(1, 1, &player)
+	var x int
+	var y int
+	nextLvl := false
 	for {
 		fmt.Scanf("%s\n", &key)
 		switch key[0] {
 		case 'w':
-			psevdoMap.movePlayer(player.x-1, player.y, &player)
+			x = player.x - 1
+			y = player.y
 		case 'a':
-			psevdoMap.movePlayer(player.x, player.y-1, &player)
+			x = player.x
+			y = player.y - 1
 		case 's':
-			psevdoMap.movePlayer(player.x+1, player.y, &player)
+			x = player.x + 1
+			y = player.y
 		case 'd':
-			psevdoMap.movePlayer(player.x, player.y+1, &player)
+			x = player.x
+			y = player.y + 1
+		case 'v':
+			nextLvl = false
+			return nextLvl
 		default:
-			return
+			x = player.x
+			y = player.y
+		}
+		if movePlayer(x, y, player, currentRoom) == true {
+			nextLvl = true
+			return nextLvl
 		}
 	}
 }
 
-//---------------- old things
-
-/*
-
-for {
-		for j := 1; j < 7; j++ {
-			for i := 1; i < 7; i++ {
-				psevdoMap.movePlayer(j, i, &player)
-				psevdoMap.writeMap()
-			}
-			for i := 6; i > 0; i-- {
-				psevdoMap.movePlayer(j, i, &player)
-				psevdoMap.writeMap()
-			}
-		}
-		for j := 6; j > 0; j-- {
-			for i := 1; i < 7; i++ {
-				psevdoMap.movePlayer(j, i, &player)
-				psevdoMap.writeMap()
-			}
-			for i := 6; i > 0; i-- {
-				psevdoMap.movePlayer(j, i, &player)
-				psevdoMap.writeMap()
-			}
-		}
-	}*/
-
-/*
-
-
-func newroom(o [3][3]obj) room {
-	return room{
-		matrix: o,
+func main() {
+	player := character{
+		x:        1,
+		y:        1,
+		typeChar: "o",
+		hp:       100,
+		pocket:   make(map[string]int),
 	}
-}
-
-
-*/
-/*
-	o00 := obj{
-		x:         0,
-		y:         0,
-		typeOfObj: "/",
+	room0 := newRoomStr([16][16]string{
+		{"■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", "⚿", " ", " ", " ", " ", "⊞", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■"},
+	})
+	room1 := newRoomStr([16][16]string{
+		{"■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "■", " ", " ", " ", "■"},
+		{"■", " ", "■", "■", " ", "■", " ", " ", " ", "■", " ", " ", " ", "■", " ", "■"},
+		{"■", " ", "⊞", "■", " ", "■", "■", " ", " ", "■", " ", "■", "■", "■", " ", "■"},
+		{"■", " ", " ", "■", " ", " ", "■", " ", " ", "■", " ", " ", "■", " ", " ", "■"},
+		{"■", " ", "■", "■", "■", " ", "■", "■", " ", "■", "■", " ", "■", " ", "■", "■"},
+		{"■", " ", " ", " ", "■", " ", " ", "■", " ", "■", "⚿", " ", "■", " ", " ", "■"},
+		{"■", "■", "■", " ", "■", "■", " ", "■", " ", "■", " ", " ", "■", "■", " ", "■"},
+		{"■", " ", " ", " ", " ", "■", " ", "■", " ", "■", " ", " ", "■", " ", " ", "■"},
+		{"■", " ", "■", "■", "■", "■", " ", "■", " ", "■", "■", "■", "■", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", "■", " ", "■", " ", "■", " ", " ", " ", " ", "■", "■"},
+		{"■", "■", "■", "■", " ", "■", " ", "■", " ", "■", " ", "■", "■", "■", "■", "■"},
+		{"■", " ", " ", " ", " ", "■", " ", "■", " ", "■", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", "■", "■", "■", "■", " ", "■", " ", "■", "■", "■", "■", "■", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■", " ", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■", "■"},
+	})
+	if game(&player, &room0) == false {
+		return
 	}
-	o01 := obj{
-		x:         0,
-		y:         1,
-		typeOfObj: "*",
+	if game(&player, &room1) == false {
+		return
 	}
-	o10 := obj{
-		x:         1,
-		y:         0,
-		typeOfObj: "*",
-	}
-	o11 := obj{
-		x:         1,
-		y:         1,
-		typeOfObj: "/",
-	}
-	matrix := [2][2]obj{
-		{o00, o01},
-		{o10, o11},
-	}
-
-	m0 := newroom(matrix)
-	m0.size = 2
-	fmt.Println(m0)
-	m0.writeMap()*/
-
-/*
-	fmt.Println("101")
 	fmt.Println("\033[2J")
-	fmt.Println("102")*/
+	fmt.Println("Win!!!!!!!!")
+}
 
-//-------------------------console clear
+//---------------- old things ---------------------------------------------------------------
+
 /*
 
-var clear map[string]func() //create a map for storing clear funcs
+	time.Sleep(time.Second / 60)
 
-func init() {
-	clear = make(map[string]func()) //Initialize it
-	clear["linux"] = func() {
-		cmd := exec.Command("clear") //Linux example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-	clear["windows"] = func() {
-		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
-		cmd.Stdout = os.Stdout
-		cmd.Run()
-	}
-}
+		{"*", "*", "*", "*", "*", "*", "*", "*"},
+		{"*", " ", "*", " ", " ", " ", " ", "*"},
+		{"*", " ", "*", " ", "*", "*", " ", "*"},
+		{"*", " ", "*", "*", "*", " ", " ", "*"},
+		{"*", " ", " ", " ", " ", " ", " ", "*"},
+		{"*", "*", "*", " ", "*", "*", " ", "*"},
+		{"*", " ", " ", " ", "*", " ", " ", "*"},
+		{"*", "*", "*", "*", "*", "*", "*", "*"},
 
-func CallClear() {
-	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-	if ok {                          //if we defined a clear func for that platform:
-		value() //we execute it
-	} else { //unsupported platform
-		panic("Your platform is unsupported! I can't clear terminal screen :(")
-	}
-}
+		{"■", "■", "■", "■", "■", "■", "■", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", " ", " ", " ", " ", " ", " ", "■"},
+		{"■", "■", "■", "■", "■", "■", "■", "■"},
 */
